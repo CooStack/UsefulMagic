b6e053e9db040ab7aa36fab8e5787633197a04fc
CooStack <yusanr114514@gmail.com>
2025-06-15 17:30:15 +0800

    修复服务器无法使用的BUG

--- src/main/kotlin/cn/coostack/usefulmagic/utils/ParticleOption.kt
+++ src/main/kotlin/cn/coostack/usefulmagic/utils/ParticleOption.kt
@@ -1,13 +1,19 @@
 package cn.coostack.usefulmagic.utils
 
+import net.fabricmc.api.EnvType
+import net.fabricmc.loader.api.FabricLoader
 import net.minecraft.client.MinecraftClient
+
 
 object ParticleOption {
     /**
      * 粒子倍数
      */
-    fun getParticleCounts(): Int =
-        3 - (MinecraftClient.getInstance()?.options?.particles?.value?.id ?: 0)
-
-
+    fun getParticleCounts(): Int {
+        // 判断环境
+        if (FabricLoader.getInstance().environmentType == EnvType.SERVER) {
+            return 3
+        }
+        return 3 - (MinecraftClient.getInstance()?.options?.particles?.value?.id ?: 0)
+    }
 }
