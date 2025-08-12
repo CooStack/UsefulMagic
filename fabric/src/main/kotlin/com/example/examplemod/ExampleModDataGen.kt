aaf63d912ebda9ba3bbd0f335b17bb50be96f72b
CooStack <yusanr114514@gmail.com>
2025-08-13 05:42:04 +0800

    it seemed works

---
+++ fabric/src/main/kotlin/com/example/examplemod/ExampleModDataGen.kt
@@ -0,0 +1,12 @@
+package com.example.examplemod
+
+import net.fabricmc.api.ClientModInitializer
+import net.fabricmc.api.ModInitializer
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object ExampleModDataGen : DataGeneratorEntrypoint {
+    override fun onInitializeDataGenerator(gen: FabricDataGenerator) {
+
+    }
+}
